308354653f8eab17e724948fb5a7a3355c714c07
Vishruth S <vs.vishruth.s@gmail.com>
2021-02-15 12:32:17 +0530

    added problemStatemnt and Prize component desktop views

--- src/Pages/HackathonPage.js
+++ src/Pages/HackathonPage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import ProblemStatement from "../Components/HackathonPage/ProblemStatement/ProblemStatement";
+import Prizes from "../Components/HackathonPage/Prizes/Prizes";
 
 export function Hackathonpage(props) {
   return (
-    <>
+    <div id="hackathonPage">
       <h1>Hackathonpage</h1>
-    </>
+      <ProblemStatement />
+      <Prizes />
+    </div>
   );
 }
