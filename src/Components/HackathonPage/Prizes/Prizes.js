9f4f3525e51bb65ad56ac5572dc0f73373a595c6
rachel1306 <60882090+rachel1306@users.noreply.github.com>
2021-02-21 00:28:43 +0530

    Navbars added

--- src/Components/HackathonPage/Prizes/Prizes.js
+++ src/Components/HackathonPage/Prizes/Prizes.js
@@ -5,29 +5,12 @@ import thirdPrizeImg from "../../../Assets/Frame-3.png";
 import "./Prizes.css";
 
 const Prizes = () => {
-<<<<<<< HEAD
   return (
     <div className="container prize-container">
       <h3 className="prize-heading">Prizes</h3>
       <div className="row">
         <div className="col m4 prize-div offset">
           <img src={secondPrizeImg} className="prize-img" alt="second prize" />
-=======
-    return (
-        <div className="container prize-container" id="prizes">
-            <h3 className="prize-heading">Prizes</h3>
-            <div className="row">
-                <div className="col m4 prize-div offset">
-                    <img src={secondPrizeImg} className="prize-img" alt="second prize" />
-                </div>
-                <div className="col m4 prize-div">
-                    <img src={firstPrizeImg} className="prize-img" alt="first prize" />
-                </div>
-                <div className="col m4 prize-div offset">
-                    <img src={thirdPrizeImg} className="prize-img" alt="third prize" />
-                </div>
-            </div>
->>>>>>> Navbars added
         </div>
         <div className="col m4 prize-div">
           <img src={firstPrizeImg} className="prize-img" alt="first prize" />
