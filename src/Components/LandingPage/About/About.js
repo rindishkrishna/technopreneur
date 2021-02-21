6a881b138ea6f10eae93bcb48cd5ca11e5823268
Ashish <ashishmp0471@gmail.com>
2021-02-21 12:40:39 +0530

    New commit

--- src/Components/LandingPage/About/About.js
+++ src/Components/LandingPage/About/About.js
@@ -3,12 +3,29 @@ import "./About.css";
 import vector from '../../../Assets/Saly-15.png'
 import downWave from '../../../Assets/Vector-3.png'
 import upWave from '../../../Assets/Vector-2.png'
+import topVector from '../../../Assets/Saly-13.png'
+import meet from '../../../Assets/meeting.jpeg'
 
 export default function AboutComponent(props){
     return(
         <div>
-        <img src={upWave} className="wave-d"/>
-        <div className = "row container">
+        <div className="top-section">
+            <img src={upWave} className="wave-d" alt="waveup"/>
+            <div className="outerTop">
+                <div className="topRow">
+                        <div className="meet-outer">
+                            <img src={meet} className="meet" alt="about vector"/>
+                        </div>
+                        <div className="vector-top">
+                            <button name="Register" className = "register-btn">Register</button>
+                        </div>
+                        <div className="vector-top">
+                            <img src={topVector} className="topVector" alt="about vector"/>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <div className = "col container">
             <div className="headerContent">
                 <p className="header">
                     About Us
@@ -26,7 +43,7 @@ export default function AboutComponent(props){
                 
             </div>
             </div>
-            <img src={downWave} className="wave-d"/>
+            <img src={downWave} className="wave-d" alt="wavedown"/>
         </div>
     )
 }
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Navbar } from "../Components/LandingPage/Navbar/Navbar";
 import Sponsors from "../Components/LandingPage/Sponsors/Sponsors";
+import AboutComponent from "../Components/LandingPage/About/About"
 
 export function Landingpage(props) {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter> 
       <Navbar />
       </BrowserRouter>
+      <AboutComponent/>
       <div id="landingPage">
       <Sponsors />
       </div>
