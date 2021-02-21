a4254493b5edfe5840f39427b8acc51b01a8f6b7
Ranjana Reghunath <ranjanareghunath.mec@gmail.com>
2021-02-21 09:39:41 +0530

    events

---
+++ src/Components/LandingPage/Events/Events.js
@@ -0,0 +1,49 @@
+import React from "react";
+import eventImg from "../../../Assets/grey-square.jpg";
+import tick from "../../../Assets/Saly-10.png";
+import "./Events.css";
+
+const Events = () => {
+  return (
+    <div className="container">
+      <div className="tick">
+        <img
+          src={tick}
+          alt=""
+          style={{ width: "25%", height: "30%", marginBottom: "4rem" }}
+        ></img>
+      </div>
+      <h1 style={{ textAlign: "center", marginBottom: "3rem" }}>Events</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <div className="event">
+          <img src={eventImg} alt="" className="eventImage"></img>
+
+          <div className="eventButton">
+            <button className="button button1">Register</button>
+          </div>
+        </div>
+        <div className="event">
+          <img src={eventImg} alt="" className="eventImage"></img>
+          <div className="eventButton">
+            <button className="button button1">Register</button>
+          </div>
+        </div>
+
+        <div className="event">
+          <img src={eventImg} alt="" className="eventImage"></img>
+          <div className="eventButton">
+            <button className="button button1">Register</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Events;
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Sponsors from "../Components/LandingPage/Sponsors/Sponsors";
+import Events from "../Components/LandingPage/Events/Events";
 
 export function Landingpage(props) {
   return (
     <div id="landingPage">
+      <Events />
       <Sponsors />
     </div>
   );
