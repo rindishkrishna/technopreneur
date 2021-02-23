64c51f66e0bb904a70b446435687342b3fc455f8
Ashish <ashishmp0471@gmail.com>
2021-02-23 10:02:26 +0530

    Fixed top section

--- src/Components/LandingPage/TopSection/TopSection.jsx
+++ src/Components/LandingPage/TopSection/TopSection.jsx
@@ -7,23 +7,29 @@ import { FaLinkedin } from 'react-icons/fa';
 import { FaTwitter }from 'react-icons/fa';
 const TopSection = () => {
     return ( 
-        <div id="home"> 
-            <img src={ logo } className="logo" alt="" />
-            <img src={ rocketImg } className="ontop" alt="" />
-            <div className="combine">
-                <h4 className="Date">Date</h4>
-                <div className="social_media">
-                <a href="https://www.instagram.com/iedcmec/?hl=en" target="_blank" rel="noopener noreferrer" className="media_page">
-                    <FaInstagram/>
-                </a>
-                <a href="https://in.linkedin.com/company/iedcmec" target="_blank" rel="noopener noreferrer" className="media_page">
-                    <FaLinkedin />
-                </a>
-                <a href="https://twitter.com/iedc_mec" target="_blank" rel="noopener noreferrer" className="media_page">
-                    <FaTwitter />
-                </a>                
+        <div id="home" className="top-container">
+            <div className="logo-section">
+                <img src= {logo} alt="LOGO" className="logo"/>
+            </div>
+            <div className="rocket-date">
+                <div className="rocket-section">
+                    <img src={rocketImg} alt="rocketman" className="rocket-man"/>
+                </div>
+                <div className="combine">
+                    <p className="Date">Date</p>
+                    <div className="social_media">
+                    <a href="https://www.instagram.com/iedcmec/?hl=en" target="_blank" rel="noopener noreferrer" className="media_page">
+                        <FaInstagram/>
+                    </a>
+                    <a href="https://in.linkedin.com/company/iedcmec" target="_blank" rel="noopener noreferrer" className="media_page">
+                        <FaLinkedin />
+                    </a>
+                    <a href="https://twitter.com/iedc_mec" target="_blank" rel="noopener noreferrer" className="media_page">
+                        <FaTwitter />
+                    </a>                
                 </div>
           </div>
+            </div>
         </div>
     )
 }
