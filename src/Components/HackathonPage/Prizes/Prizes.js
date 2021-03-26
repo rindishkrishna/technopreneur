44ca33d39046547329773bc570e5cb1ccb22f898
rindish krishna <rindishkrishna@gmail.com>
2021-03-26 23:25:23 +0530

    Prizes update

--- src/Components/HackathonPage/Prizes/Prizes.js
+++ src/Components/HackathonPage/Prizes/Prizes.js
@@ -1,29 +1,35 @@
 import React from "react";
-import firstPrizeImg from "../../../Assets/Frame-1.png";
-import secondPrizeImg from "../../../Assets/Frame-2.png";
-import thirdPrizeImg from "../../../Assets/Frame-3.png";
+import portis from "../../../Assets/Portis.png";
+import polygon from "../../../Assets/Polygon.png";
+import tezos from "../../../Assets/Tezos.png";
 import "./Prizes.css";
 
 const Prizes = () => {
   return (
     <div className="container prize-container" id="prizes">
       <h3 className="heading">Prizes</h3>
-      <div className="col m4 prize-div">
-          {/* <h2 >Prizes Worth </h2>
-          <h3>3 Lakhs</h3> */}
-          <img src={firstPrizeImg} className="prize-img" alt="first prize" />
-          <h3>7000 ₹</h3>
-          </div>
+
       <div className="row">
+      <div className="col m4 prize-div">
+          <img src={tezos} className="prize-img" alt="first prize" />
+          <h3>20,000 ₹</h3>
+          <br></br>
+          <h4>Best Hack built on Tezos</h4>
+        </div>
         <div className="col m4 prize-div ">
-          <img src={secondPrizeImg} className="prize-img" alt="second prize" />
-          <h3>5000 ₹</h3>
+          <img src={portis} className="prize-img" alt="second prize" />
+          <h3>15,000 ₹</h3>
+          <br></br>
+          <h4>Best Hack built on Portis</h4>
+        </div>
+
+        <div className="col m4 prize-div ">
+          <img src={polygon} className="prize-img" alt="third prize" />
+          <h3>10,000 ₹</h3>
+          <br></br>
+          <h4>Best Hack built on Polygon</h4>
         </div>
         
-        <div className="col m4 prize-div ">
-          <img src={thirdPrizeImg} className="prize-img" alt="third prize" />
-          <h3>2000 ₹</h3>
-        </div>
       </div>
     </div>
   );
--- src/Components/HackathonPage/ProblemStatement/ProblemStatement.js
+++ src/Components/HackathonPage/ProblemStatement/ProblemStatement.js
@@ -6,7 +6,7 @@ const ProblemStatement = () => {
   return (
     <div className="row container" id="problemstatement">
       <div className="col m8">
-        <h3 className="heading">Problem Statement</h3>
+        <h3 className="heading">Tracks</h3>
         <div className="problem-content-div">
           <p className="problem-text">
           ⚪ Open Innovation
