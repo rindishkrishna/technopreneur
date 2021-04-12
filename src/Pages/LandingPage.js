4946091859ddcaf3e9a82d3cf0f6d01d322d8aec
Vivek R <123vivekr@gmail.com>
2021-04-12 22:26:50 +0530

    smooth scroll + scroll to top

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TopSection from "../Components/LandingPage/TopSection/TopSection";
 import Sponsors from "../Components/LandingPage/Sponsors/Sponsors";
 import Events from "../Components/LandingPage/Events/Events";
@@ -6,6 +6,7 @@ import Events from "../Components/LandingPage/Events/Events";
 import AboutComponent from "../Components/LandingPage/About/About";
 import ContactUs from "../Components/LandingPage/ContactUs/ContactUs";
 import ReactPageScroller from 'react-page-scroller';
+import {FaArrowCircleUp} from 'react-icons/fa';
 
 export function Landingpage(props) {
   const [currentPage, setCurrentPage] = useState(null)
@@ -18,12 +19,42 @@ export function Landingpage(props) {
     console.log(number);
   }
 
+  const [showScroll, setShowScroll] = useState(false);
+
+  useEffect(()=>{
+    window.addEventListener('scroll', checkScrollTop)
+    return function cleanup() {
+      window.removeEventListener('scroll', checkScrollTop)
+    }
+  })
+  
+  
+  const checkScrollTop = () => {
+    if (currentPage > 0){
+      setShowScroll(true)
+    } else {
+      setShowScroll(false)
+    }
+  };
+  useEffect(checkScrollTop, [currentPage]);
+
+  const scrollTop = () =>{
+    console.log("hy")
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    setCurrentPage(0);
+  };
+
   return (
     <>
+      <div style={{position:"absolute", bottom:10, right:20, zIndex:10000}}>
+        <FaArrowCircleUp onClick={scrollTop} style={{height: 50, width:"50px", display: showScroll? 'flex' : 'none'}}/>
+      </div>
       <ReactPageScroller
         pageOnChange={handlePageChange}
         onBeforePageScroll={handleBeforePageChange}
         customPageNumber={currentPage}
+        animationTimer={500}
+        animationTimerBuffer={10}
       >
         <TopSection />    
         <AboutComponent/>
